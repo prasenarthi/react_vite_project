9db85ee031f18e6a76ca2197ac6f5416ed3cfcdb
prasen aarthi <prasen.aarthi@gmail.com>
2024-01-19 14:54:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-center text-red-800">
+        Welcome to React Project
+      </h1>
+    </div>
+  );
+}
+
+export default App;
